48d0e5ab31c53936e0b1fb92adda5a78a29084a3
Matej Vavrinec <wmatex@gmail.com>
2016-06-05 21:17:21 +0200

    First playable version

--- js/Field.js
+++ js/Field.js
@@ -1,9 +1,36 @@
 var app;
 app = (function (app) {
-    var MINE_PROB = 0.1; // 10% chance of generating a mine
+    var MINE_PROB = 0.2; // 10% chance of generating a mine
     var X_LENGTH = 1.7320508; // sqrt(3)
+    var HEX_SIZE = null;
 
-    app.game.field = function (width, height) {
+    app.game.field = function (canvasWidth, canvasHeight, width, height) {
+        if (canvasWidth > canvasHeight) {
+            if (height > width) {
+                var tmp = width;
+                width = height;
+                height = tmp;
+            }
+        } else {
+            if (width > height) {
+                var tmp = width;
+                width = height;
+                height = tmp;
+            }
+        }
+
+        this._cW = canvasWidth;
+        this._cH = canvasHeight;
+
+        if (canvasWidth / width < canvasHeight / height) {
+            var x = 2*canvasWidth/(2*width + 1);
+            HEX_SIZE = x/X_LENGTH;
+        } else {
+            HEX_SIZE = 2 * canvasHeight / (3*height + 1);
+        }
+
+        console.log(HEX_SIZE, X_LENGTH);
+
         this._width = width;
         this._height = height;
         this._data = [];
@@ -73,7 +100,25 @@ app = (function (app) {
         }
     };
 
+    app.game.field.prototype.getUnderCursor = function(x, y) {
+        var yIndex = Math.floor(y/(HEX_SIZE*1.5));
+        var xIndex = Math.floor(x/(X_LENGTH*HEX_SIZE)) - 1;
+
+        for (var i = 0; i < 2; i++) {
+            yIndex += i;
+            for (var j = 0; j < 2; j++) {
+                xIndex += j;
+                if (yIndex < this._height && xIndex >= 0 && xIndex < this._width) {
+                    if (this._data[yIndex][xIndex].inside(x,y)) {
+                        return this._data[yIndex][xIndex];
+                    }
+                }
+            }
+        }
+    };
+
     app.game.field.prototype.render = function (ctx) {
+        ctx.clearRect(0, 0, this._cW, this._cH);
         for (var y = 0; y < this._data.length; y++) {
             for (var x = 0; x < this._data[y].length; x++) {
                 this._data[y][x].render(ctx);
@@ -84,32 +129,58 @@ app = (function (app) {
     app.game.field.tile = function (y, x) {
         this._y = y;
         this._x = x;
+
+        this._clicked = false;
+
+        var offset = this._y % 2 == 1 ? this.rel(X_LENGTH/2.0) : 0;
+        this._origin = [offset+this._x*this.rel(X_LENGTH), this.rel(0.5) + this.rel(1.5*this._y)];
+        this._points = [
+            [0 , 0],
+            [ Math.round(this.rel(X_LENGTH/2.0)), Math.round(-this.rel(0.5)) ],
+            [ Math.round(this.rel(X_LENGTH)), Math.round(this.rel(0)) ],
+            [ Math.round(this.rel(X_LENGTH)), Math.round(this.rel(1)) ],
+            [ Math.round(this.rel(X_LENGTH/2.0)), Math.round(this.rel(1.5)) ],
+            [ Math.round(this.rel(0)), Math.round(this.rel(1)) ],
+        ];
     };
 
     app.game.field.tile.constructor = app.game.field.tile;
 
-    app.game.field.tile.prototype._size = 100;
+
+    app.game.field.tile.prototype.inside = function(x, y) {
+        function sign(x1, y1, x2, y2, x, y) {
+            return y - y1 - (y2 - y1)/(x2 - x1)*(x - x1);
+        }
+
+        for (var i = 1; i < this._points.length; i++) {
+            if (!sign(this._points[i-1][0], this._points[i-1][0], this._points[i][0], this._points[i][1], x-this._origin[0], y-this._origin[1])) {
+                return false;
+            }
+        }
+        return x > this._origin[0] && x < this._origin[0] + X_LENGTH*HEX_SIZE;
+    };
 
     app.game.field.tile.prototype.render = function (ctx) {
-        ctx.fillStyle = "black";
+        ctx.strokeStyle = "blue";
+        ctx.lineWidth = 1;
 
-        var offset = this._y % 2 == 1 ? this.rel(X_LENGTH/2.0) : 0;
-        var oX = offset+this._x*this.rel(X_LENGTH), oY = this.rel(0.5) + this.rel(1.5*this._y);
-
+        ctx.translate(this._origin[0], this._origin[1]);
         ctx.beginPath();
-        ctx.moveTo(Math.round(oX), Math.round(oY));
+        ctx.moveTo(this._points[0][0], this._points[0][1]);
 
-        ctx.lineTo(Math.round(oX + this.rel(X_LENGTH/2.0)), Math.round(oY - this.rel(0.5)));
-        ctx.lineTo(Math.round(oX + this.rel(X_LENGTH)), Math.round(oY + this.rel(0)));
-        ctx.lineTo(Math.round(oX + this.rel(X_LENGTH)), Math.round(oY + this.rel(1)));
-        ctx.lineTo(Math.round(oX + this.rel(X_LENGTH/2.0)), Math.round(oY + this.rel(1.5)));
-        ctx.lineTo(Math.round(oX + this.rel(0)), Math.round(oY + this.rel(1)));
+        for (var i = 1; i < this._points.length; i++) {
+            ctx.lineTo(this._points[i][0], this._points[i][1]);
+        }
+        ctx.closePath();
 
-        ctx.fill();
+        ctx.stroke();
     };
 
     app.game.field.tile.prototype.rel = function(val) {
-        return val * this._size;
+        return val * HEX_SIZE;
+    };
+    app.game.field.tile.prototype.click = function(field) {
+        this._clicked = true;
     };
 
     app.game.field.mine = function (y, x) {
@@ -118,8 +189,22 @@ app = (function (app) {
     app.game.field.mine.prototype = Object.create(app.game.field.tile.prototype);
     app.game.field.mine.constructor = app.game.field.mine;
     app.game.field.mine.prototype.render = function (ctx) {
+        ctx.save();
         app.game.field.tile.prototype.render.call(this, ctx);
+
+        if (this._clicked) {
+            ctx.fillStyle = "red";
+            ctx.fill();
+        }
+        ctx.restore();
     };
+
+    app.game.field.mine.prototype.click = function(auto, field) {
+        if (!auto) {
+            throw "Game Over";
+        }
+    };
+
 
     app.game.field.numbertile = function (y, x, number) {
         app.game.field.tile.call(this, y, x);
@@ -128,7 +213,31 @@ app = (function (app) {
     app.game.field.numbertile.prototype = Object.create(app.game.field.tile.prototype);
     app.game.field.numbertile.constructor = app.game.field.numbertile;
     app.game.field.numbertile.prototype.render = function (ctx) {
+        ctx.save();
         app.game.field.tile.prototype.render.call(this, ctx);
+
+        if (this._clicked && this._n > 0) {
+            ctx.font = HEX_SIZE + "px arial";
+            ctx.textBaseline = "middle";
+            ctx.textAlign = "center";
+            ctx.fillText(this._n, this.rel(X_LENGTH / 2.0), this.rel(0.5));
+        }
+
+
+        ctx.restore();
+    };
+
+    app.game.field.numbertile.prototype.click = function(auto, field) {
+        app.game.field.tile.prototype.click.call(this, auto, field);
+
+        if (this._n < 1) {
+            var neighbours = field._data.getNeighbourhood(this._y, this._x);
+            for (var i = 0; i < neighbours.length; i++) {
+                if (!neighbours[i]._clicked) {
+                    neighbours[i].click(true, field);
+                }
+            }
+        }
     };
 
     return app;
--- js/Game.js
+++ js/Game.js
@@ -1,11 +1,43 @@
 var app = (function(app) {
 
     // Game constructor
-    app.game = function( level) {
+    app.game = function(container, level) {
         this._level = level || this.level.SMALL;
-        this._field = new app.game.field(level[0], level[1]);
+        this._canvas = _createGameCanvas.call(this, container);
+        this._field = new app.game.field(this._canvas.width, this._canvas.height, level[0], level[1]);
 
-        this._field.render(document.querySelector('.game canvas').getContext('2d'));
+        this._field.render(this._canvas.getContext('2d'));
+    };
+
+    function _createGameCanvas(container) {
+        var header = document.querySelector('header');
+        var footer = document.querySelector('footer');
+
+        var canvas = document.createElement('canvas');
+        canvas.width = container.clientWidth;
+        console.log(document.body.clientHeight, header.clientHeight, footer.clientHeight);
+        canvas.height = document.body.clientHeight - header.clientHeight - footer.clientHeight;
+        container.style.height = canvas.height + "px";
+
+        var self = this;
+        canvas.addEventListener('click', this._click.bind(this));
+        container.appendChild(canvas);
+        return canvas;
+    };
+
+    app.game.prototype._click = function(e) {
+        var bound = e.target.getBoundingClientRect();
+        var tile = this._field.getUnderCursor(e.clientX-bound.left, e.clientY-bound.top);
+        if (tile !== undefined) {
+            if (tile instanceof app.game.field.mine) {
+                alert("Game over man!");
+            } else {
+                tile.click(false, this._field);
+            }
+
+            this._field.render(this._canvas.getContext('2d'));
+        }
+
     };
 
     app.game.level = {
